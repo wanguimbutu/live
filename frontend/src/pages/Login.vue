7ecda1a78db0f1256ffef49f0680277c24289044
wanguimbutu <wanguimbutu@gmail.com>
2025-01-22 17:45:53 +0300

    chane in login

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,5 +1,4 @@
 <template>
-  <ion-page>
   <div class="m-3 flex flex-row items-center justify-center">
     <Card title="Login to Frappe Live!" class="w-full max-w-md mt-4">
       <form class="flex flex-col space-y-2 w-full" @submit.prevent="submit">
@@ -23,7 +22,7 @@
       </form>
     </Card>
   </div>
-  </ion-page>
+  
 </template>
 
 <script lang="ts" setup>
